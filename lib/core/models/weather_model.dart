c0e3c1f52be9240fb5a2edfa4fe55cbe4293acde
Ehab Mostafa <hobamostafa205@gmail.com>
2026-01-06 21:54:32 +0400

    weather model done

--- lib/core/models/weather_model.dart
+++ lib/core/models/weather_model.dart
@@ -1,8 +1,10 @@
 class WeatherModel {
+  final String cityName;
+  final String date;
   final double tempC;
   final double tempF;
   final String conditionName;
-  final String conditionIconUrl;
+  final String? conditionIconUrl;
   final int humidity;
   final double windSpeed;
   final double feelsLikeC;
@@ -15,10 +17,12 @@ class WeatherModel {
   // final String day;
 
   WeatherModel({
+    required this.cityName,
+    required this.date,
     required this.tempC,
     required this.tempF,
     required this.conditionName,
-    required this.conditionIconUrl,
+    this.conditionIconUrl,
     required this.humidity,
     required this.windSpeed,
     required this.feelsLikeC,
@@ -31,18 +35,20 @@ class WeatherModel {
 
   factory WeatherModel.fromJson(Map<String, dynamic> json) {
     return WeatherModel(
-      tempC: json['docId'],
-      tempF: json['medicineUsage'],
-      conditionName: json['medicineName'],
-      conditionIconUrl: json['frequency'],
-      humidity: json['medicineNotes'],
-      windSpeed: json['isReminderActive'],
-      feelsLikeC: json['medicineTypes'],
-      feelsLikeF: json['imageUrl'],
-      minTempC: json['imageUrl'],
-      minTempF: json['imageUrl'],
-      maxTempC: json['imageUrl'],
-      maxTempF: json['imageUrl'],
+      cityName: json['location']['name'],
+      date: json['current']['last_updated'],
+      tempC: json['current']['temp_c'],
+      tempF: json['current']['temp_f'],
+      conditionName: json['current']['condition']['text'],
+      conditionIconUrl: json['current']['condition']['icon'],
+      humidity: json['current']['humidity'],
+      windSpeed: json['current']['wind_kph'],
+      feelsLikeC: json['current']['feelslike_c'],
+      feelsLikeF: json['current']['feelslike_f'],
+      minTempC: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+      minTempF: json['forecast']['forecastday'][0]['day']['mintemp_f'],
+      maxTempC: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+      maxTempF: json['forecast']['forecastday'][0]['day']['maxtemp_f'],
     );
   }
 }
