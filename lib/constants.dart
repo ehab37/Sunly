5ff624e7ccf9d122e94aa30842a114fddeec8f2f
Ehab Mostafa <hobamostafa205@gmail.com>
2026-01-06 17:29:40 +0400

    Include a .env for API keys

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ApiKeys {
+  static final String apiKey = dotenv.env['WEATHER_API_KEY'] ?? 'No Key Found';
+  static const String baseUrl = "http://api.weatherapi.com/v1/";
+}
+
+const Color kPrimaryColor = Color(0xff5cb6f9);
+const kTransitionDuration = Duration(milliseconds: 400);
+LinearGradient disco = const LinearGradient(
+  colors: [Color(0xFF4568DC), Color(0xFFB06AB3)],
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:go_transitions/go_transitions.dart';
 import 'core/utils/app_router.dart';
 
-void main() {
+void main() async {
+  // Load the .env file
+  await dotenv.load(fileName: ".env");
   runApp(const Sunly());
 }
 
